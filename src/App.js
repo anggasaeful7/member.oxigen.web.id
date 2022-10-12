c63305c7940c8ebcaf2e128660dde6bf76221de9
anggasaeful7 <anggasaeful3@gmail.com>
2022-10-12 16:34:07 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -5,29 +5,6 @@ function App() {
   return (
     <div>
       <Bganimasi />
-      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-      <label htmlFor="my-modal" className="btn modal-button">
-        open modal
-      </label>
-
-      <input type="checkbox" id="my-modal" className="modal-toggle" />
-      <div className="modal">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">
-            Congratulations random Internet user!
-          </h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
-          <div className="modal-action">
-            <label htmlFor="my-modal" className="btn">
-              Yay!
-            </label>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 }
